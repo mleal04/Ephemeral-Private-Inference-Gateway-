ae550d71ad2aff7332e4380be2f23b0ee4909c68
Maria Leal <lealm1199@gmail.com>
2026-02-24 17:58:14 -0500

    file deletions

--- part1/ipc_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef IPC_UTILS_H
-#define IPC_UTILS_H
-
-// int create_message_queue();
-void send_to_worker(int pipefd, char *request_buffer, int bytes);
-void read_from_worker(int pipefd, char *response_buffer, int buffer_size);
-
-#endif // IPC_UTILS_H
