6f87aca4dfb612dbb4ef403b383379ba68fc4a76
Maria Leal <lealm1199@gmail.com>
2026-05-28 15:20:11 -0500

    adding the attestion first

--- part1/client.py
+++ part1/client.py
@@ -1,11 +1,23 @@
 import requests
 
-url = "https://127.0.0.1:8443"
+url_to_attestation = "https://127.0.0.1:8443/attestation"
+url_to_gateway = "https://127.0.0.1:8443"
+url_to_relay = None
+pcc_node_pub_rek = None  
 
 try:
-    #make the GET request
-    message = "Hello from the Client!"
-    answer = requests.get(url, data=message, verify=False)  # Set verify=False to ignore SSL warnings for self-signed certs
-    print(f"Response from server: {answer.text}")
+
+    #make the attestion request to PCC (straight TLS (Client to Gateway))
+    message = "ATTESTATION_REQUEST"
+    answer = requests.get(url_to_attestation, data=message, verify=False)  # Set verify=False to ignore SSL warnings for self-signed certs
+    print(f"Response from PCC: {answer.text}")
+    
+
+    # #encrypt the message with REK
+
+
+    # #make the request to get the PCC response (HTTP to relay --> OHTTPS from relay to gateway)
+
+
 except Exception as e:
     print(f"An error occurred: {e}")
--- part1/worker_process.c
+++ part1/worker_process.c
@@ -11,15 +11,50 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-void ShutdownSSL(SSL *cSSL);
+#include "worker_process.h"
+
+
+void pcc_attestion(char *response_buffer) {
+    char *rek_pub_key = getenv("rek_pub_key");
+    if (rek_pub_key != NULL) {
+        snprintf(response_buffer, BUFSIZ,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: application/x-pem-file\r\n"
+                "Content-Length: %lu\r\n"
+                "\r\n"
+                "%s", 
+                strlen(rek_pub_key), rek_pub_key);
+    } else {
+        snprintf(response_buffer, BUFSIZ,
+                "HTTP/1.1 500 Internal Server Error\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: %lu\r\n"
+                "\r\n"
+                "Failed to load REK.", 
+                strlen("Failed to load REK."));
+    }
+}
+
+
 void pcc_node_logic(SSL *cSSL, int new_socket) {
     char request_buffer[BUFSIZ];
     int bytes = SSL_read(cSSL, request_buffer, sizeof(request_buffer));
     request_buffer[bytes] = '\0';
-    //send request to right right nodes fucntions 
     printf("%s\n", request_buffer);
+
+    //check for variety of requests and respond accordingly
+    char response[BUFSIZ];
+
+    char *prefix = "/attestation";
+    // Change strncmp to strstr
+    if (strstr(request_buffer, prefix) != NULL) {
+        pcc_attestion(response); // Make sure to pass your buffer size!
+    } else {
+        snprintf(response, sizeof(response),
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from the PCC node!");
+    }
+
     //SSL write response back to client
-    char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from the PCC node!";
     SSL_write(cSSL, response, strlen(response));
     ShutdownSSL(cSSL);
     close(new_socket);
